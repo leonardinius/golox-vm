940e8e0f91048dccafb0809c1b992ca5d1d7aab3
Leo <leonidms@gmail.com>
2024-06-20 00:52:50 +0300

    readline repl

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+
+	"github.com/chzyer/readline"
 
 	"github.com/leonardinius/goloxvm/internal/chunk"
 	"github.com/leonardinius/goloxvm/internal/debug"
@@ -19,5 +23,26 @@ func main() {
 
 	ch.FreeChunk()
 	fmt.Println("main > ", strings.Join(args, " "))
+	_ = repl()
+
 	os.Exit(0)
+}
+
+func repl() error {
+	rl, err := readline.New("> ")
+	if err != nil {
+		return err
+	}
+	defer func() { _ = rl.Close() }()
+
+	for {
+		line, err := rl.Readline()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Printf("<< %s\n", line)
+	}
 }
