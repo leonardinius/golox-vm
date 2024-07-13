6de4641e75f6faaabe3b8323831933678a122259
Leo <leonidms@gmail.com>
2024-07-13 13:07:06 +0300

    renames

--- internal/vmcompiler/compiler.go
+++ internal/vmcompiler/compiler.go
@@ -1,50 +1,28 @@
 package vmcompiler
 
 import (
-	"fmt"
-
 	"github.com/leonardinius/goloxvm/internal/vmchunk"
-	"github.com/leonardinius/goloxvm/internal/vmvalue"
 )
 
 func Compile(source []byte, chunk *vmchunk.Chunk) error {
 	scanner := NewScanner(source)
 	defer scanner.Free()
 
-	parseTokens(scanner)
-
-	AddBinOp(chunk, vmchunk.OpAdd, 2.0, 3.0)
-	chunk.WriteOpcode(vmchunk.OpReturn, 1)
+	advance()
+	expression()
+	consume(TokenEOF, "Expect end of expression.")
 
 	return nil
 }
 
-func parseTokens(scanner Scanner) {
-	line := -1
-	for {
-		token := scanner.ScanToken()
-		if token.Line != line {
-			line = token.Line
-			fmt.Printf("%04d ", token.Line)
-		} else {
-			fmt.Print("  |  ")
-		}
-		fmt.Printf("[%-17s] '%s'\n", token.Type, token.Lexeme())
-
-		// for now just print the tokens
-		if token.Type == TokenEOF {
-			break
-		}
-	}
+func advance() {
+	panic("unimplemented")
 }
 
-func AddBinOp(chunk *vmchunk.Chunk, op vmchunk.OpCode, a, b vmvalue.Value) {
-	aConstant := chunk.AddConstant(a)
-	bConstant := chunk.AddConstant(b)
+func expression() {
+	panic("unimplemented")
+}
 
-	chunk.WriteOpcode(vmchunk.OpConstant, 1)
-	chunk.Write(byte(aConstant), 1)
-	chunk.WriteOpcode(vmchunk.OpConstant, 1)
-	chunk.Write(byte(bConstant), 1)
-	chunk.WriteOpcode(op, 1)
+func consume(tk TokenType, message string) {
+	panic("unimplemented")
 }
---
+++ internal/vmcompiler/parser.go
@@ -0,0 +1,8 @@
+package vmcompiler
+
+// type Parser struct {
+// 	current   Token
+// 	previous  Token
+// 	hadError  bool
+// 	panicMode bool
+// }
