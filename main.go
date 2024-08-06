fd704afdcd312b5d94bba025e792e21b3ecd40f1
Leo <leonidms@gmail.com>
2024-08-06 23:23:41 +0300

    add pprof profiler

--- main.go
+++ main.go
@@ -5,13 +5,61 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/chzyer/readline"
 
 	"github.com/leonardinius/goloxvm/internal/vm"
+	"github.com/leonardinius/goloxvm/internal/vm/vmdebug"
 )
 
 func main() {
+	os.Exit(mainApp())
+}
+
+func mainApp() int {
+	cfgPproff := vmdebug.LoadPprofConfigFromEnv()
+
+	if cfgPproff.On && cfgPproff.CPUProfile != "" {
+		f, err := os.Create(cfgPproff.CPUProfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR] could not create CPU profile: %#v", err)
+			return 1
+		}
+		defer ioClose(f)
+		if err := pprof.StartCPUProfile(f); err != nil {
+			ioClose(f)
+			fmt.Fprintf(os.Stderr, "[ERROR] could not start CPU profile: %#v", err)
+			return 1
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	code := mainCli()
+
+	if cfgPproff.On && cfgPproff.MemProfile != "" {
+		f, err := os.Create(cfgPproff.MemProfile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[ERROR] could not create memory profile: %#v", err)
+			return code
+		}
+		defer ioClose(f)
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			ioClose(f)
+			fmt.Fprintf(os.Stderr, "[ERROR] could not write memory profile: %#v", err)
+			return code
+		}
+	}
+
+	return code
+}
+
+// mainCli is the entry point for the CLI
+// It handles the command line arguments and calls the appropriate functions
+// It also initializes and frees the VM.
+func mainCli() int {
 	args := os.Args[1:]
 	vm.InitVM()
 
@@ -23,17 +71,17 @@ func main() {
 		err = runFile(args[0])
 	} else {
 		fmt.Printf("Usage: %s [path]\n", filepath.Base(os.Args[0]))
-		os.Exit(64)
+		return 64
 	}
 
 	vm.FreeVM()
 
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(65)
+		return 65
 	}
 
-	os.Exit(0)
+	return 0
 }
 
 func repl(welcome string) error {
@@ -70,6 +118,6 @@ func runFile(script string) error {
 
 func ioClose(c io.Closer) {
 	if err := c.Close(); err != nil {
-		fmt.Printf("[WARN] close: %s\n", err)
+		fmt.Fprintf(os.Stderr, "[WARN ] close: %s\n", err)
 	}
 }
